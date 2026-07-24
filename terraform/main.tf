59f4b35284e90b683bc1508897b51b7127925088
Pavan <pavanmotupalli4@gmil.com>
2026-07-24 14:07:10 +0530

    Enable EBS encryption and EC2 security best practices

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,33 @@
 resource "aws_instance" "jenkins_server" {
 
   ami                    = var.ami_id
-
   instance_type          = var.instance_type
-
   key_name               = var.key_name
 
-  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+  vpc_security_group_ids = [
+    aws_security_group.jenkins_sg.id
+  ]
 
-  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+
+  monitoring = true
+
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
+
+  root_block_device {
+    volume_size           = 20
+    volume_type           = "gp3"
+    encrypted             = true
+    delete_on_termination = true
+  }
 
   tags = {
-
-    Name = "Capstone-Server"
-
+    Name        = "Capstone-Server"
+    Environment = "Dev"
+    Project     = "Capstone"
   }
 
 }
